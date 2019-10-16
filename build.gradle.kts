2cff0e159fbcfabf9b23616c7cd07ca12f28a859
Andreas Lingenhag <11538311+alingenhag@users.noreply.github.com>
2019-10-16 18:59:04 +0200

    Use latest version of plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     kotlin("jvm") version "1.3.50"
     id("java-gradle-plugin")
     id("com.gradle.plugin-publish") version "0.10.1"
-    id("ch.fuzzle.gradle.semver") version "0.1.3-ci+4d4547af"
+    id("ch.fuzzle.gradle.semver") version "0.1.3-ci+312c73cd"
 }
 
 description = "gradle semantic versioning plugin"
