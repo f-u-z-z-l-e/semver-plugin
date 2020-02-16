d7f908d69056abe13ae73322d04e6754e0a5bfcd
alingenhag <11538311+alingenhag@users.noreply.github.com>
2020-02-16 18:08:41 +0100

    Update to gradle 6.1.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("jvm") version "1.3.50"
     id("java-gradle-plugin")
     id("com.gradle.plugin-publish") version "0.10.1"
-    id("ch.fuzzle.gradle.semver") version "0.3.1"
+    id("ch.fuzzle.gradle.semver") version "0.3.3"
     jacoco
     id("pl.droidsonroids.jacoco.testkit") version "1.0.5"
 }
