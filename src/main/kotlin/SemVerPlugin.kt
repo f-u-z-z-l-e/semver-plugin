cf92b67a414fe01ac3a05c71c68e666ae17e2ca7
Beat Kappert <beat.kappert1@swisscom.com>
2022-06-07 21:10:21 +0200

    resolveNextVersion in gradle.afterProject instead of afterEvaluate (front-run the maven-publish plugin)

--- src/main/kotlin/SemVerPlugin.kt
+++ src/main/kotlin/SemVerPlugin.kt
@@ -11,21 +11,24 @@ class SemVerPlugin : Plugin<Project> {
         with(project) {
             extensions.create("semver", SemVerExtension::class.java, project)
 
-            afterEvaluate {
+            tasks.register("tagHeadCommit", TagHeadCommitTask::class.java)
+            tasks.register("displayVersion", DisplayVersion::class.java)
+
+            // this task needs git to be configured outside of gradle.
+            tasks.register("pushTagToOrigin", Exec::class.java) { task ->
+                task.group = "Versioning"
+                task.description = "Pushes the tags to origin."
+                task.commandLine = listOf("git", "push", "origin", "--tags")
+            }
+
+            /**
+             * If we do this in afterEvaluate, this plugin must come before the maven-publish plugin in the Gradle build
+             * script. Doing it in afterProject is more robust, no need to pay attention to the sequence of plugins.
+             */
+            gradle.afterProject {
                 val nextVersionTask = tasks.create("nextVersion", NextVersionTask::class.java)
                 nextVersionTask.resolveNextVersion()
-
-                tasks.register("tagHeadCommit", TagHeadCommitTask::class.java)
-                tasks.register("displayVersion", DisplayVersion::class.java)
-
-                // this task needs git to be configured outside of gradle.
-                tasks.register("pushTagToOrigin", Exec::class.java) { task ->
-                    task.group = "Versioning"
-                    task.description = "Pushes the tags to origin."
-                    task.commandLine = listOf("git", "push", "origin", "--tags")
-                }
             }
         }
     }
-
 }
